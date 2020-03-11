473c65686bbec8e5e7d70bd1686fd6ce62bf13df
Vitor Alves Mendes <vmendes@establishmentlabs.com>
2020-03-11 13:54:52 -0300

    Added Redis and Queue

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import Appointment from '../models/Appointment'
 import File from '../models/File'
 import Notification from '../schema/Notification'
 
-import Mail from '../../lib/Mail'
+import CancellationMail from '../jobs/CancellationMail'
+import Queue from '../../lib/Queue'
 
 class AppointmentController {
   async index (req, res) {
@@ -111,7 +112,8 @@ class AppointmentController {
   async delete (req, res) {
     const appointment = await Appointment.findByPk(req.params.id, {
       include: [
-        { model: User, as: 'provider', attributes: ['name', 'email'] }
+        { model: User, as: 'provider', attributes: ['name', 'email'] },
+        { model: User, as: 'user', attributes: ['name'] }
       ]
     })
 
@@ -133,10 +135,8 @@ class AppointmentController {
 
     await appointment.save()
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      text: 'Você tem um novo cancelamento'
+    await Queue.add(CancellationMail.key, {
+      appointment
     })
 
     return res.status(200).json({
